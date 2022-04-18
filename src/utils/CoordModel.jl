3fe8f76ca75d7e868382ca2ed962ebb26dc09515
take.sei <sei06k14@gmail.com>
2022-04-18 17:54:55 +0900

    :sparkles: Add some util functions

--- src/utils/CoordModel.jl
+++ src/utils/CoordModel.jl
@@ -1,3 +1,53 @@
+function padding(x::Matrix{U}) where U
+    field = zeros(U, size(x) .+ 2)
+    field[begin+1:end-1, begin+1:end-1] = x
+    return field
+end
+
+function padding(x::Vector{U}) where U
+    field = zeros(U, size(x) .+ 2)
+    field[begin+1:end-1] = x
+    return field
+end
+
+supress(x::Matrix{U}) where U = x[begin+1:end-1, begin+1:end-1]
+supress(x::Vector{U}) where U = x[begin+1:end-1]
+
+function periodicbc!(x::Matrix{T}) where T
+    x[begin, :] = x[end-1, :]
+    x[end, :] = x[begin+1, :]
+    x[:, begin] = x[:, end-1]
+    x[:, end] = x[:, begin+1]
+    return x
+end
+
+function periodicbc!(x::Vector{T}) where T
+    x[begin] = x[end-1]
+    x[end] = x[begin+1]
+    return x
+end
+
+mutable struct CoordStatus{T} <: BaseVar{T}
+    value::Matrix{T}
+    function CoordStatus(x::Matrix{U}; bc::Function = x -> x) where U 
+        field = padding(x)
+        field = bc(field)
+        return new{U}(field)
+    end
+end
+
+mutable struct ArrayCoordStatus{T} <: ArrayVar{T}
+    value::Vector{Matrix{T}}
+    function ArrayCoordStatus(x::Vector{Matrix{U}}; bc::Function = x->x) where U
+        field = map(x) do s
+            field = padding(s)
+            field = bc(field)
+            return field
+        end
+        return new{U}(field)
+    end
+end
+
 Base.@kwdef struct CoordModel{T<:BaseParams} <: BaseModel{T}
     iter::Function
     params::T
@@ -7,17 +57,27 @@ Base.@kwdef struct CoordModel{T<:BaseParams} <: BaseModel{T}
     n_iter::UInt64
 end
 
-function laplacian(s::Matrix{T})::Matrix{T} where {T<:Real}
-    res = zeros(T, size(s))
-    res[begin:end-1, :] += @view s[begin+1:end, :]
-    res[begin+1:end, :] += @view s[begin:end-1, :]
-    res[:, begin:end-1] += @view s[:, begin+1:end]
-    res[:, begin+1:end] += @view s[:, begin:end-1]
-    res -= 4 * s
-    return res
+function laplacian(s::Matrix{T})::Matrix{T} where T
+    @views return (
+        s[begin+2:end, begin+1:end-1]
+        + s[begin:end-2, begin+1:end-1]
+        + s[begin+1:end-1, begin+2:end]
+        + s[begin+1:end-1, begin:end-2]
+        - 4 * s[begin+1:end-1, begin+1:end-1]
+   )
 end
 
+function laplacian(s::Vector{T})::Vector{T} where T
+    @views return (
+        s[begin+2:end]
+        + s[begin:end-2]
+        - 2 * s[begin+1:end-1]
+   )
+end
+
+
 Δ(s::Matrix{T}) where {T<:Real} = laplacian(s)
+Δ(s::Vector{T}) where {T<:Real} = laplacian(s)
 
 function start(mod::CoordModel; kwargs...)::Vector
     dt = haskey(kwargs, :dt) ? kwargs[:dt] : (mod.dt)
@@ -25,9 +85,8 @@ function start(mod::CoordModel; kwargs...)::Vector
     save_by = haskey(kwargs, :save_by) ? kwargs[:save_by] : (mod.save_by)
     n_iter = haskey(kwargs, :n_iter) ? kwargs[:n_iter] : (mod.n_iter)
 
-    res = Vector(undef, div(n_iter, save_by)+1)
-
     st = copy(mod.params)
+    res = Vector{typeof(save(st))}(undef, div(n_iter, save_by)+1)
     res[begin] = save(st)
 
     @progress for i in 1:n_iter
@@ -40,4 +99,12 @@ function start(mod::CoordModel; kwargs...)::Vector
     return res
 end
 
-export CoordModel, Δ, laplacian, start
+export CoordModel,
+       Δ,
+       laplacian,
+       start,
+       CoordStatus,
+       padding,
+       periodicbc!,
+       ArrayCoordStatus,
+       supress
--- test/utils/coordmodel_test.jl
+++ test/utils/coordmodel_test.jl
@@ -1,20 +1,59 @@
 using sudareph, Test, InteractiveUtils
 
+@testset "padding" begin
+    @test [1;] |> padding == [0, 1, 0]
+    @test [1;;] |> padding == [0 0 0; 0 1 0; 0 0 0]
+    @test [1, 2] |> padding == [0, 1, 2, 0]
+    @test [1 2; 3 4] |> padding == [0 0 0 0; 0 1 2 0; 0 3 4 0; 0 0 0 0]
+end
+
+@testset "supress" begin
+    @test [1;] |> padding |> supress == [1;]
+    @test [1;;] |> padding |> supress == [1;;]
+    @test [1, 2] |> padding |> supress == [1, 2]
+    @test [1 2; 3 4] |> padding |> supress == [1 2; 3 4]
+end
+
+@testset "periodic boundary condition" begin
+    @test [1;] |> padding |> periodicbc! == ones(3)
+    @test [1;;] |> padding |> periodicbc! == ones(3, 3)
+    @test [1, 2] |> padding |> periodicbc! == [2, 1, 2, 1]
+    @test [1 2; 3 4] |> padding |> periodicbc! == [4 3 4 3; 2 1 2 1; 4 3 4 3; 2 1 2 1]
+end
+
 case = [
     (arg=zeros(3, 3), exp=zeros(3, 3)),
     (arg=ones(3, 3),
         exp=[-2 -1 -2; -1 0 -1; -2 -1 -2]),
-    (arg=Matrix(reshape(1:9, 3, 3)),
+    (arg=Matrix(reshape(1:1.0:9, 3, 3)),
         exp=[2 1 -4; -3 0 -7; -16 -11 -22]'),
 ]
 
-@testset "laplacian" begin
+@testset "Coord Status & laplacian" begin
     for (i, c) in enumerate(case)
         @testset "case $i" begin
             a, e = c[:arg], c[:exp]
-            @test laplacian(a) == e
-            @test Δ(a) == e
+            @test_nowarn CoordStatus(a)
+            temp = CoordStatus(a)
+            @test temp.value == a |> padding
+            @test laplacian(temp.value) == e
+            @test Δ(temp.value) == laplacian(temp.value)
+
+            t = CoordStatus(a; bc=periodicbc!)
+            @test t.value == a |> padding |> periodicbc!
         end
+    end
+    @testset "case Array" begin
+        a = Vector{Matrix{Float64}}([case[i].arg for i in 1:length(case)])
+        e = Vector{Matrix{Float64}}([case[i].exp for i in 1:length(case)])
+        @test_nowarn ArrayCoordStatus(a)
+        temp = ArrayCoordStatus(a)
+        @test temp.value == map(padding, a)
+        @test (@. laplacian(temp.value) == e) |> all
+        @test (@. Δ(temp.value) == laplacian(temp.value)) |> all
+
+        t = ArrayCoordStatus(a; bc=periodicbc!)
+        @test t.value == map(x -> x |> padding |> periodicbc!, a)
     end
 end
 
@@ -40,6 +79,10 @@ sample = CoordModel{CoordParams}(params |> copy, 1, 1, 1, 3) do st, dt, dx
 end
 
 @testset "Simulating" begin
+    @testset "Benchmark" begin
+        # @show @code_warntype start(sample)
+        # @show @code_warntype start(sample; n_iter=3, save_by=1)
+    end
     @testset "General Usage" begin
         res = start(sample)
         @test length(res) == 4
